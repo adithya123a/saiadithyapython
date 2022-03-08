24c1b9f8e8f5f7677377a9981f1018612edaad2e
adithya123a <101180434+adithya123a@users.noreply.github.com>
2022-03-08 16:09:42 +0530

    Update and rename README.md to operations.py

---
+++ operations.py
@@ -0,0 +1,12 @@
+# saiadithyapython
+a=int(input("Enter a number1:"))
+b=int(input("Enter a number2:"))
+c=int(input("Enter a number3:"))
+sum=a+b+c
+product=a*b*c
+diff=a-b-c
+division=a/b/c
+print("sum of given number is;",sum)
+print("product of given number is;",product)
+print("diff of given number is;",diff)
+print("division of given number is;",division)
